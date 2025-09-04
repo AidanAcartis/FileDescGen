01dfa6e585fae1ab0ef62d8c2d65ce47794743e5
AidanAcartis <ralaikotonadia@gmail.com>
2025-09-05 00:01:40 +0300

    got the 1468th lines

--- Collect_info/Collect_file/scrap_description.py
+++ Collect_info/Collect_file/scrap_description.py
@@ -45,7 +45,7 @@ try:
             # -----------------------------
             # Ignorer les entrées déjà traitées
             # -----------------------------
-            if int(entry["id"]) < 955:
+            if int(entry["id"]) < 1394:
                 continue
 
             fname = entry["filename"]
